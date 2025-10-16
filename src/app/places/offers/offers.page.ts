0786433287b8d3a5e9d08aa6d2430a358180c5ce
Thomas Edson <andepipous@gmail.com>
2025-10-16 19:55:04 -0400

    feat: create offers module. This module render the offers that user has. It has a ion-item-sliding to interact with the rows

---
+++ src/app/places/offers/offers.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../models/places.model';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.page.html',
+  styleUrls: ['./offers.page.scss'],
+  standalone: false,
+})
+export class OffersPage implements OnInit {
+
+  loadedOffers!: Place[];
+
+  constructor(
+    private placesService: PlacesService,
+    private router: Router,
+  ) { }
+
+
+  ngOnInit() {
+    this.loadedOffers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/places/offers/edit', offerId]);
+  }
+}
