419304a50c50620d19c9e558d8c428b8faf533cd
Thomas Edson <andepipous@gmail.com>
2026-02-14 14:43:40 -0300

    config: add capacitor configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'app-book',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000
+    }
+  },
+  server: {
+    cleartext: true
+  }
 };
 
 export default config;
